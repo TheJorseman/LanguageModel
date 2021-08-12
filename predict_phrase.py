c582490226a4ef23293bc4df055c7595f72a0629
miguel17546mals <miguel17546mals@gmail.com>
2021-08-11 23:10:16 -0500

    [ADD] probability phrase

---
+++ predict_phrase.py
@@ -0,0 +1,52 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+from tools.clean_corpus import clean_raw_corpus
+from tools.bos_eos import insert_bos_eos, get_reduced_corpus, out_of_vocabulary_token
+from tools.vocabulary import create_vocabulary
+from tools.bigrams import create_bigrams
+# Data
+from NeuralNetwork.data.language import LanguageDataset
+from NeuralNetwork.data.dataloader import DataLoader
+# Modelos
+from NeuralNetwork.models.linear import Linear
+from NeuralNetwork.models.sequential import Sequential
+# Loss
+from NeuralNetwork.loss.crossentropy import CrossEntropyLoss
+# Act F
+from NeuralNetwork.functions.tanh import Tanh
+from NeuralNetwork.functions.relu import ReLU
+from NeuralNetwork.models.batch_normalization import BatchNorm1d
+from NeuralNetwork.functions.softmax import Softmax
+# Optimizer 
+from NeuralNetwork.optimizer.sgd import SGD
+from NeuralNetwork.optimizer.vanillagd import VanillaGD
+import pickle
+import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("phrase", help="Frase que se quiere calcular la probabilidad",
+                    type=str)
+args = parser.parse_args()
+
+loss = CrossEntropyLoss()
+
+def calc_probability(vocabulary, one_hot_m, model, words):
+  corpus = clean_raw_corpus([words])
+  default = vocabulary.get(out_of_vocabulary_token)
+  probability = 1.0
+  for word in corpus.split(" "):
+    vec = one_hot_m[vocabulary.get(word, default)]
+    output = model(vec).squeeze()
+    probability *= output[np.argmax(output)]
+  return probability
+
+
+dataset_test = pickle.load(open("test_dataset", "rb"))
+vocabulary = pickle.load(open("vocabulary", "rb"))
+model = pickle.load(open("model-final.bin", "rb"))
+
+one_hot = np.identity(len(vocabulary))
+
+probability = calc_probability(vocabulary, one_hot, model, args.phrase)
+print(probability)
